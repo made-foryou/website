5ac1187f2487a2837764e76807492a13681cde75
Menno Tempelaar <menno@made-foryou.nl>
2025-01-27 10:16:29 +0100

    Add social links

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -52,9 +52,6 @@
                                    class="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
                                     Nieuwsgierig en kennismaken?
                                 </a>
-                                <a href="https://www.linkedin.com/in/menno-tempelaar/" class="text-sm/6 font-semibold text-gray-900">
-                                    Of bekijk mijn linkedin <span aria-hidden="true">→</span>
-                                </a>
                             </div>
                         </div>
                         <img src="{{ url('/assets/image/menno-tempelaar.JPG') }}" alt="Menno Tempelaar" class="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36">
@@ -80,6 +77,21 @@
                                         <dd><a class="font-semibold text-indigo-600" href="mailto:menno@made-foryou.nl">menno@made-foryou.nl</a></dd>
                                     </div>
                                 </dl>
+                                <div class="flex justify-end items-center space-x-2">
+                                    <a href="https://github.com/mennotempelaar" target="_blank" class="text-gray-600 hover:text-gray-800">
+                                        <span class="sr-only">GitHub</span>
+                                        <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                            <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
+                                        </svg>
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/menno-tempelaar/" target="_blank" class="text-gray-600 hover:text-gray-800">
+                                        <span class="sr-only">LinkedIn</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="size-6" fill="currentColor">
+                                        <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                        <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
+                                        </svg>
+                                    </a>
+                                </div>
                             </div>
                             <div class="rounded-2xl bg-gray-50 p-10">
                                 <h3 class="text-base/7 font-semibold text-gray-900">Muriël Baars</h3>
@@ -89,6 +101,15 @@
                                         <dd><a class="font-semibold text-indigo-600" href="mailto:muriel@made-foryou.nl">muriel@made-foryou.nl</a></dd>
                                     </div>
                                 </dl>
+                                <div class="flex justify-end items-center space-x-2">
+                                    <a href="https://www.linkedin.com/in/muriel-seo-marketing-communicatie/" target="_blank" class="text-gray-600 hover:text-gray-800">
+                                        <span class="sr-only">LinkedIn</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="size-6" fill="currentColor">
+                                        <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                        <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
+                                        </svg>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -98,15 +119,15 @@
         <footer class="bg-white">
             <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
                 <div class="flex justify-center gap-x-6 md:order-2">
-                    <a href="https://github.com/mennotempelaar" class="text-gray-600 hover:text-gray-800">
+                    <a href="https://github.com/made-foryou" target="_blank" class="text-gray-600 hover:text-gray-800">
                         <span class="sr-only">GitHub</span>
                         <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
                         </svg>
                     </a>
-                    <a href="https://www.linkedin.com/in/menno-tempelaar/" class="text-gray-600 hover:text-gray-800">
+                    <a href="https://www.linkedin.com/company/made-foryou/" target="_blank" class="text-gray-600 hover:text-gray-800">
                         <span class="sr-only">LinkedIn</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="size-6" fill="currentColor""">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="size-6" fill="currentColor">
                             <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
                             <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
                         </svg>
